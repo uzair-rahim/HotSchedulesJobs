bce58ab82151497a2337a39c274783aa80a5b356
Uzair Rahim <uzair.rahim@redbookconnect.com>
2014-05-20 13:56:28 -0500

    Employer Profile

--- scripts/views/view-employer-profile.js
+++ scripts/views/view-employer-profile.js
@@ -15,7 +15,8 @@ define([
 		className : "content",
 		template: Template,
 		events : {
-			"click #save-settings" : "saveSettings"
+			"click #save-settings"	: "saveSettings",
+			"click #upload-logo"	: "uploadLogo"
 		},
 
 		initialize : function(){
@@ -23,37 +24,89 @@ define([
 			console.log("Employer profile view initialized...");
 		},
 
+		uploadLogo : function(){
+			console.log("upload");
+		},
+
 		saveSettings : function(){
-			var employer = new Object();
-				employer.id = this.model.id;
-				employer.guid = this.model.guid;
-				employer.name = $("#name").val();
-				employer.url = $("#website").val();
-				employer.phone = $("#phone").val();
 
-			var address = new Object();	
-			
-				address.id = this.model.location.id;
-				address.address1 = $("#street").val();	
-				address.city = $("#city").val();
-				address.state = $("#state").text();
-				address.zip = $("#zip").val();
+			$("input").removeClass("error");
 
-				employer.location = address;
+			var vldtRegister = vldt.validate({
+				"#name"			: "alpha",
+				"#street"		: "alphanumeric",
+				"#city"			: "alpha",
+				"#zip"			: "zip",
+				"#phone"		: "alphanumeric",
+				"#website"		: "alphanumeric"
+			});
 
-				var employerGUIDs = Utils.GetUserSession().employerIds;
+			if(!vldtRegister){
+				
+				var errors = vldt.getErrors();
+				console.log(vldt.getErrors());
 
-				var profile = new EmployerProfile({guid : employerGUIDs[0]});
-
-				profile.save(employer, {
-					success : function(){
-						App.router.controller.profileSettings();
-					},
-					error : function(){
-						console.log("Error saving employer profile...");
-						Utils.ShowToast({ message : "Error employer profile..."});
+				for(var key in errors){
+					if(errors[key] === false){
+						$(key).addClass("error");
+						switch(key){
+							case "#name":
+								Utils.ShowToast({message : "There are errors in the form..."});
+							break;
+							case "#street":
+								Utils.ShowToast({message : "There are errors in the form..."});
+							break;	
+							case "#city":
+								Utils.ShowToast({message : "There are errors in the form..."});
+							break;
+							case "#zip":
+								Utils.ShowToast({message : "There are errors in the form..."});
+							break;
+							case "#phone":
+								Utils.ShowToast({message : "There are errors in the form..."});
+							break;
+							case "#website":
+								Utils.ShowToast({message : "There are errors in the form..."});
+							break;
+						}
 					}
-				});
+				}
+
+				return false;
+
+			}else{
+				var employer = new Object();
+					employer.id = this.model.id;
+					employer.guid = this.model.guid;
+					employer.name = $("#name").val();
+					employer.url = $("#website").val();
+					employer.phone = $("#phone").val();
+
+				var address = new Object();	
+				
+					address.id = this.model.location.id;
+					address.address1 = $("#street").val();	
+					address.city = $("#city").val();
+					address.state = $("#state").text();
+					address.zip = $("#zip").val();
+
+					employer.location = address;
+
+					var employerGUIDs = Utils.GetUserSession().employerIds;
+
+					var profile = new EmployerProfile({guid : employerGUIDs[0]});
+
+					profile.save(employer, {
+						success : function(){
+							App.router.controller.profileSettings();
+						},
+						error : function(){
+							console.log("Error saving employer profile...");
+							Utils.ShowToast({ message : "Error employer profile..."});
+						}
+					});
+
+			}
 
 		},
 

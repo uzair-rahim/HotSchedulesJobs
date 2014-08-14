60aba7708d96fae5d15c9793c18c0640f1867e77
Uzair Rahim <uzair.rahim@redbookconnect.com>
2014-08-14 11:27:21 -0500

    User work history

---
+++ scripts/models/model-user.js
@@ -0,0 +1,70 @@
+define([
+	"backbone",
+	"utils"
+	],
+	function(Backbone, Utils){
+		var User = Backbone.Model.extend({
+
+			defaults : {
+				id 					: null,
+				firstname 			: null,
+				lastname 			: null,
+				email 				: null,
+				guid 				: null,
+				about 				: null,
+				verified 			: null,
+				followedEmployerIds : null,
+				endorsedEmployerIds : null,
+				identities 			: null,
+				emails 				: null,
+				password 			: null,
+				placesWorkedCount 	: null,
+				returning 			: null,
+				employerIds 		: null,
+				adminEmployers 		: null,
+				photo 				: null,
+				primaryWorkHistory 	: null,
+				workHistory 		: null,
+				roles 				: null,
+				location 			: null,
+				accountState 		: null,
+			},
+
+			initialize : function(){
+				console.log("User model initialize...");
+			},
+
+			url : function(){
+				var url = this.urlRoot();
+				return url;
+			},
+
+			urlRoot : function(){
+				return Utils.GetURL("/services/rest/user");
+			},
+
+			getWorkHistory : function(callback){
+				var that = this;
+				var guid = this.attributes.guid;
+				var url = this.urlRoot() + "/" + guid + "/workHistory";
+				
+				$.ajax({
+					type 	: "GET",
+					url		: url,
+					success : function(response){
+						that.set({workHistory : response.history});
+						callback();
+					},
+					error : function(model, errors){
+						console.log("Error fetching user work history");
+						Utils.ShowToast({message : "Error fetching user work histroy"});
+					}
+				})
+
+			},
+
+		});
+
+		return User;
+	}
+);
